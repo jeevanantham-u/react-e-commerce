f176aec375a329896ae2a06c6caad5d90d277e52
jeevanantham-u <jeevanantham2k2@gmail.com>
2024-07-23 00:08:19 +0530

    footer alignment updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,15 +9,18 @@ const Footer = () => {
     <footer className="footer">
       <Container>
         <Row>
-          <Col>
+          <Col lg="6" md="12">
           <div className='footer__logo'>
             <img src={logo} alt="Logo" />
           </div>
-            <i class="ri-facebook-circle-fill">u</i>
+          <span className="footer__icons">
+            <i class="ri-facebook-circle-fill"></i>
             <i class="ri-linkedin-fill"></i>
+          </span>
+            
           </Col>
-          <Col>
-            <p>
+          <Col lg="6" md="12" >
+            <p className="footer__content">
               Do you believe in sustainable future too?
               Get in touch with us via <Link to={"contact@aatral-esp.com"}
               >contact@aatral-esp.com</Link>
